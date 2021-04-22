c583ad72d578317fc77c2b93319f2b23e7d06448
yangsai <yangsair@gmail.com>
2021-04-22 14:21:42 +0800

    修复文件检查问题

--- source/uCEFMiscFunctions.pas
+++ source/uCEFMiscFunctions.pas
@@ -10,7 +10,7 @@
 // For more information about CEF4Delphi visit :
 //         https://www.briskbard.com/index.php?lang=en&pageid=cef
 //
-//        Copyright © 2019 Salvador Diaz Fau. All rights reserved.
+//        Copyright ?2019 Salvador Diaz Fau. All rights reserved.
 //
 // ************************************************************************
 // ************ vvvv Original license and comments below vvvv *************
@@ -1062,8 +1062,8 @@ begin
       TempList.Add(TempDir + 'd3dcompiler_47.dll');
       TempList.Add(TempDir + 'libEGL.dll');
       TempList.Add(TempDir + 'libGLESv2.dll');
-      TempList.Add(TempDir + 'swiftshader\libEGL.dll');
-      TempList.Add(TempDir + 'swiftshader\libGLESv2.dll');
+      //TempList.Add(TempDir + 'swiftshader\libEGL.dll');
+      //TempList.Add(TempDir + 'swiftshader\libGLESv2.dll');
       TempList.Add(TempDir + 'icudtl.dat');
 
       if TempExists then
